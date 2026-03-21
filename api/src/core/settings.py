e6d78a9cf2fc333e77e629ebc81155d39718614f
AlexandreCotorobai <alexandrecotorobai@gmail.com>
2026-03-21 14:58:49 +0000

    Dev mode

--- api/src/core/settings.py
+++ api/src/core/settings.py
@@ -13,6 +13,11 @@ class Settings(BaseSettings):
     MONGODB_URI: str
     MONGODB_DB_NAME: str = "risktool"
     API_IAM_SERVER_URL: str | None = None
+    API_DEV_MODE: bool = False
+    API_DEV_USER_ID: str = "dev-user"
+    API_DEV_USERNAME: str = "dev"
+    API_DEV_EMAIL: str = "dev@example.com"
+    API_DEV_IS_ADMIN: bool = True
 
 
 settings = Settings()  # type: ignore
